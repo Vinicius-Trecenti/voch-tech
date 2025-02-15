7145aca8c303175ec735324336ebc2ca4a7a430d
Vinicius Trecenti <vinitrecenti@gmail.com>
2025-02-15 15:47:23 -0300

    Install TallStackUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './vendor/tallstackui/tallstackui/src/**/*.php',
     ],
 
     theme: {
@@ -16,6 +17,10 @@ export default {
             },
         },
     },
+
+    presets: [
+        require('./vendor/tallstackui/tallstackui/tailwind.config.js')
+    ],
 
     plugins: [forms],
 };
