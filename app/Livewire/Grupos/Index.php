950bf0578472c53738b6b032ea9ce85e0aeaf7ab
Vinicius Trecenti <vinitrecenti@gmail.com>
2025-02-16 11:44:47 -0300

    Fix CRUD grupos

--- app/Livewire/Grupos/Index.php
+++ app/Livewire/Grupos/Index.php
@@ -5,14 +5,15 @@ namespace App\Livewire\Grupos;
 use App\Models\Grupo;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Illuminate\Database\Eloquent\Builder;
 
 class Index extends Component
 {
     use WithPagination;
 
-    // public ?int $quantity = 10;
+    public ?int $quantity = 10;
 
-    // public ?string $search = null;
+    public ?string $search = null;
 
     public array $headers = [
         ['index' => 'id', 'label' => 'ID'],
@@ -22,11 +23,54 @@ class Index extends Component
         ['index' => 'actions', 'label' => 'Ações'],
     ];
 
+    public $showModalEdit = false;
+    public $grupo;
+    public $nome;
+    public $showModalDelete = false;
+    public $grupoNome = '';
+
+    public function openModalEdit($row)
+    {
+        $this->grupo = $row;
+        $this->nome = $row['nome'];
+        $this->showModalEdit = true;
+    }
+
+    public function openModalDelete($row)
+    {
+        $this->grupoNome = $row['nome'];
+        $this->showModalDelete = true;
+    }
+
+    public function edit()
+    {
+        $this->validate([
+            'nome' => 'required|min:3',
+        ],
+        [
+            'nome.required' => 'O campo nome é obrigatório',
+            'nome.min' => 'O campo nome deve ter no mínimo 3 caracteres',
+        ]);
+
+        Grupo::where('id', $this->grupo['id'])->update([
+            'nome' => $this->nome
+        ]);
+
+        $this->showModalEdit = false;
+        $this->reset('nome');
+
+        redirect(route('grupos'))->with('success', 'Grupo atualizado com sucesso');
+    }
 
     public function render()
     {
         return view('livewire.grupos.index', [
-            'rows' => Grupo::all(),
+            'rows' => Grupo::query()
+                ->when($this->search, function (Builder $query) {
+                    return $query->where('nome', 'like', "%{$this->search}%");
+                })
+                ->paginate($this->quantity)
+                ->withQueryString(),
         ]);
     }
 }
--- resources/views/livewire/grupos/index.blade.php
+++ resources/views/livewire/grupos/index.blade.php
@@ -1,11 +1,64 @@
 <div class="p-4">
-    <x-ts-table :$headers :$rows id="grupos">
+    <x-ts-table :$headers :$rows filter id="grupos">
         @interact('column_actions', $row)
             <div class="flex gap-2">
-                <livewire:grupos.edit :$row />
+                <x-ts-button icon="pencil" position="left" outline wire:click="openModalEdit({{ $row }})"/>
 
-                <livewire:grupos.delete :$row />
+                <x-ts-button icon="trash" color="red" position="left" outline wire:click="openModalDelete({{ $row }})"/>
             </div>
         @endinteract
     </x-ts-table>
+
+
+    {{-- Modal de Edição --}}
+    <div x-data="{ open: @entangle('showModalEdit') }" x-show="open" x-transition.opacity class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+        <div class="bg-white p-6 rounded-lg w-96">
+            <h2 class="text-xl font-semibold mb-4 text-black">Editar grupo</h2>
+
+            <div class="grid grid-cols-12 gap-4">
+                <div class="col-span-12">
+                    <x-ts-errors />
+                </div>
+
+                <div class="col-span-12">
+                    <x-ts-input label="Nome do grupo" wire:model="nome" />
+                </div>
+            </div>
+
+            <div class="flex justify-center space-x-4 mt-4">
+                <x-ts-button color="gray" x-on:click="open = false">
+                    {{ __('Cancelar') }}
+                </x-ts-button>
+
+                <x-ts-button wire:click="edit">
+                    {{ __('Salvar') }}
+                </x-ts-button>
+            </div>
+        </div>
+    </div>
+
+    {{-- Modal de Exclusão --}}
+    <div x-data="{ open: @entangle('showModalDelete') }" x-show="open" x-transition.opacity class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+        <div class="bg-white p-6 rounded-lg w-96">
+            <h2 class="text-xl font-semibold mb-4 text-black">Deletar grupo</h2>
+
+            <div class="grid grid-cols-12 gap-4">
+                <div class="col-span-12">
+                    <p>Tem certeze que deseja deletar?</p>
+                    <p class="text-black my-4">O grupo <span class="font-bold">{{ $grupoNome }} </span> será deletado.</p>
+
+                </div>
+            </div>
+
+            <div class="flex justify-center space-x-4 mt-4">
+                <x-ts-button color="gray" x-on:click="open = false">
+                    {{ __('Cancelar') }}
+                </x-ts-button>
+
+                <x-ts-button wire:click="delete" color="red">
+                    {{ __('Confirmar') }}
+                </x-ts-button>
+            </div>
+        </div>
+    </div>
 </div>
