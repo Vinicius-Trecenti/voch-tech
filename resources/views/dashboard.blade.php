43e584088d6164a8b7a04395c74e77e31d6105ba
Vinicius Trecenti <vinitrecenti@gmail.com>
2025-02-17 18:19:31 -0300

    Fix bug chart loading on Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -38,60 +38,76 @@
 
 
 <script>
-  var dados = @json($dados);
-  const ctx = document.getElementById('colaboradoresByUnidade');
-
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: dados.map(unidade => unidade.unidade),
-      datasets: [{
-        label: 'Colaboradores por Unidade',
-        data: dados.map(unidade => unidade.colaboradores),
-        backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(201, 203, 207, 0.2)'
-        ],
-        borderColor: [
-        'rgb(255, 99, 132)',
-        'rgb(255, 159, 64)',
-        'rgb(75, 192, 192)',
-        'rgb(54, 162, 235)',
-        'rgb(153, 102, 255)',
-        'rgb(201, 203, 207)'
-        ],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
+  function iniciarCharts(){
+    const existeBarChart = Chart.getChart("colaboradoresByUnidade");
+    if(existeBarChart){
+      existeBarChart.destroy();
     }
-  });
 
-</script>
+    const existeDonutChart = Chart.getChart("colaboradoresByUnidadeDonut");
+    if(existeDonutChart){
+      existeDonutChart.destroy();
+    }
 
+    const barCtx = document.getElementById('colaboradoresByUnidade');
+    if(barCtx){
+      new Chart(barCtx, {
+        type: 'bar',
+        data: {
+          labels: window.chartData.map(unidade => unidade.unidade),
+          datasets: [{
+            label: 'Colaboradores por unidade',
+            data: window.chartData.map(unidade => unidade.colaboradores),
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      });
+    }
 
-<script>
-  var dadosDonut = @json($dados);
-  const cty = document.getElementById('colaboradoresByUnidadeDonut');
+    const donutCtx = document.getElementById('colaboradoresByUnidadeDonut');
+    if(donutCtx){
+      new Chart(donutCtx, {
+        type: 'doughnut',
+        data: {
+          labels: window.chartData.map(unidade => unidade.unidade),
+          datasets: [{
+            label: 'Colaboradores',
+            data: window.chartData.map(unidade => unidade.colaboradores),
+            borderWidth: 1
+          }]
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: false
+        }
+      });
+    }
+  }
 
-  new Chart(cty, {
-    type: 'doughnut',
-    data: {
-      labels: dadosDonut.map(unidade => unidade.unidade),
-      datasets: [{
-        label: 'Colaboradores por Unidade',
-        data: dadosDonut.map(unidade => unidade.colaboradores),
-        borderWidth: 1
-      }]
-    },
-  });
+  window.chartData = @json($dados);
+  document.addEventListener('DOMContentLoaded', iniciarCharts);
+  document.addEventListener('livewire:navigated', iniciarCharts);
 </script>
